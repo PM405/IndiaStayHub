d896dfb2517a2ed044c9acd44198727547a139c6
Prince Mishra <pm6152271@gmail.com>
2026-04-19 11:16:44 +0530

    Refactor app.js to improve code structure

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/wanderlust
 //const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL)
 }
 
 main()
